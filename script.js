09c1f184ce6c9dfb14d4a1a4685913784d730e65
Rafax34 <rafaaydinfadhilah@gmail.com>
2026-08-29 15:09:53 +0700

    Link View buttons to member profile pages

--- script.js
+++ script.js
@@ -354,7 +354,7 @@ function renderMembers() {
       <div class="info">
         <div class="name">${i + 1}. ${m.name}</div>
         <div class="role" data-translate="${m.roleKey}">${t(m.roleKey)}</div>
-        <button class="member-view-btn">View</button>
+        <a href="members-profile/${encodeURIComponent(m.name)}/index.html" class="member-view-btn">View</a>
       </div>
     </article>
   `).join('');
